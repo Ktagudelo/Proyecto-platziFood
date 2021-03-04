eef8336c96e71ab383870d005306c3adddd427f8
Ktagudelo <47460810+Ktagudelo@users.noreply.github.com>
2021-03-03 22:37:41 -0500

    Project Platzi Food

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Proyecto-platziFood
+
+Creación de Platzi Food, el sitio web de un negocio de comida. Con Tailwind, se realizó las mejores prácticas, 
+como diseñar primero para móviles y optimizar la página para cualquier dispositivo.
+
+HTML, CSS, Tailwind
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        'primary': "#f3c614",
+        'secondary':"#353535"
+      },
+      height:{
+        '80': '28rem',
+        '100': '25rem'
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+};
